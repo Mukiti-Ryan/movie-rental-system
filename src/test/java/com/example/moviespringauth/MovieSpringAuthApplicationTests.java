55765be9c47abf92a0675314a473520bd6303293
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-03-21 11:57:22 +0300

    Tests

---
+++ src/test/java/com/example/moviespringauth/MovieSpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviespringauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSpringAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
