3acf4dd9dba34761bbf679e38074ca79cca3f917
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-03-21 12:39:41 +0300

    Staff entity updated

--- src/main/java/com/example/moviespringauth/Entities/Staff.java
+++ src/main/java/com/example/moviespringauth/Entities/Staff.java
@@ -14,6 +14,7 @@ import static javax.persistence.FetchType.EAGER;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table
 public class Staff {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
