f040d0d895f33db1883e11074e2dfda2deeacfc7
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-03-21 11:55:24 +0300

    Role and Staff entities update

---
+++ src/main/java/com/example/moviespringauth/Entities/Staff.java
@@ -0,0 +1,29 @@
+package com.example.moviespringauth.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static javax.persistence.FetchType.EAGER;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+    @ManyToMany(fetch = EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
