1fbbd538da4ee0599ac279c7b9fd36fd39f09eb4
Mukiti-Ryan <87067381+Mukiti-Ryan@users.noreply.github.com>
2022-03-21 12:35:24 +0300

    Address entity created and updated

---
+++ src/main/java/com/example/moviespringauth/Entities/Address.java
@@ -0,0 +1,40 @@
+package com.example.moviespringauth.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long addressId;
+    private String address;
+    private String address2;
+    private Long district;
+    private String postalCode;
+    private String phoneNumber;
+    private Timestamp lastUpdate;
+
+    @OneToMany
+    private Collection<Staff> staffs = new ArrayList<>();
+
+    @OneToMany
+    private Collection<Customer> customers = new ArrayList<>();
+
+    @OneToMany
+    private Collection<Store> stores = new ArrayList<>();
+
+    @ManyToOne
+    private City city1;
+}
